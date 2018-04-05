4f734f3c3c92e865c4014c5ea94d6c2c948150ba
Nick-Damico <nickalan82@icloud.com>
2018-04-04 22:57:50 -0400

    claned up code

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 // AUTHOR: NICHOLAS D'AMICO NICKALAN82@ICLOUD.COM
 // NICHOLASDAMICO.NET
 // SEPT 21 2016
+// Updated April 4 2018
 
 var photo = document.querySelectorAll(".photo");
 var thumbnail = document.querySelectorAll(".thumbnail");
@@ -27,25 +28,25 @@ var photoArr = [];
 
 // LIGHTBOX CLOSE
 function close() {
-    // lightBox.style.visibility = "hidden";
-    Velocity(lightBox, "fadeOut", 800);
+  // lightBox.style.visibility = "hidden";
+  Velocity(lightBox, "fadeOut", 800);
 }
 
 // LIGHTBOX CHANGE TO NEXT PIC BTN.
 function nextPic() {
-    if (counter === photoArr.length - 1) {
-      counter = 0;
-    } else {
-      counter++;
-    }
+  if (counter === photoArr.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
 
-    while (!photoArr[counter].active) {
-      counter++;
-      if (counter === photo.length) {
-        counter = 0;
-      }
+  while (!photoArr[counter].active) {
+    counter++;
+    if (counter === photo.length) {
+      counter = 0;
     }
-    modalDisplay(photoArr[counter]);
+  }
+  modalDisplay(photoArr[counter]);
 }
 
 // LIGHTBOX CHANGE TO PREV PIC BTN.
@@ -67,22 +68,22 @@ function prevPic() {
 
 // UPDATES LIGHTBOX IMG AND DESCRIPTION, CALL FROM prevPic() or nextPic();
 function modalDisplay(photo) {
-    // UPDATE LIGHTBOX IMG SRC TAG FROM .PHOTO
-    lightboxImg.setAttribute("src", photo.src);
-    // UPDATE LIGHTBOX CAPTION W/ ALT TAG OF .THUMBNAIL
-    lightboxAbout.textContent = photo.text;
+  // UPDATE LIGHTBOX IMG SRC TAG FROM .PHOTO
+  lightboxImg.setAttribute("src", photo.src);
+  // UPDATE LIGHTBOX CAPTION W/ ALT TAG OF .THUMBNAIL
+  lightboxAbout.textContent = photo.text;
 }
 
 function filter(search) {
-var liItem = '';
-const wrapperEl = document.querySelector('.wrapper');
-for (var i = 0; i < photoArr.length; i++) {
+  var liItem = '';
+  const wrapperEl = document.querySelector('.wrapper');
+  for (var i = 0; i < photoArr.length; i++) {
     liItem = cols[i];
     // CACHE ARRAY TEXT : VALUE STORED.
     var queryText = photoArr[i].text;
     var queryTitle = photoArr[i].title;
 
-    if(queryText.indexOf(search) >= 0 || queryTitle.indexOf(search) >= 0 ) {
+    if (queryText.indexOf(search) >= 0 || queryTitle.indexOf(search) >= 0) {
       liItem.style.display = 'initial';
       photoArr[i].active = true;
     } else {
@@ -95,7 +96,7 @@ for (var i = 0; i < photoArr.length; i++) {
 function getPhotoId(photo) {
   let selectedPhoto = photoArr.filter(p =>
     p.title === photo.firstElementChild.title.toLowerCase());
-    return selectedPhoto[0].id;
+  return selectedPhoto[0].id;
 }
 
 
@@ -111,48 +112,48 @@ function getPhotoId(photo) {
 
 // SEARCH BAR EVENT HANDLER ON KEYUP
 searchBar.addEventListener("keyup", function() {
-    filter(this.value.trim().toLowerCase());
+  filter(this.value.trim().toLowerCase());
 });
 
 
 // LOOP ADDS EVENT HANDLER "CLICK" TO img.thumbnails & a.photo.
 (function() {
-        for (var i = 0; i < photo.length; i++) {
+  for (var i = 0; i < photo.length; i++) {
 
-        // ADD EVENT "CLICK"
-        photo[i].addEventListener("click", function() {
-            // PREVENTS A LINK FROM OPENING URL, STOP DEFAULT BEHAVIOR.
-            event.preventDefault();
-            counter = getPhotoId(this);
-            // UPDATES LIGHTBOX IMG TO CLICKED IMAGE.
-            elLightBoxImg.setAttribute(
-              "src",
-              this.firstElementChild.getAttribute('data-full-image')
-            );
-        });
-        // CLICK OPENS LIGHTBOX MODAL.
-        thumbnail[i].addEventListener("click", function() {
-            // TURNS LIGHTBOX 'ON' WITH IMG 'CLICK'.
-            lightBox.style.visibility = "visible";
-            Velocity(lightBox, "fadeIn", 800);
-            // UPDATES LIGHTBOX CAPTION TEXT FROM 'CLICKED' IMG [ATTR="ALT"]
-            lightboxAbout.textContent = this.getAttribute("alt");
+    // ADD EVENT "CLICK"
+    photo[i].addEventListener("click", function() {
+      // PREVENTS A LINK FROM OPENING URL, STOP DEFAULT BEHAVIOR.
+      event.preventDefault();
+      counter = getPhotoId(this);
+      // UPDATES LIGHTBOX IMG TO CLICKED IMAGE.
+      elLightBoxImg.setAttribute(
+        "src",
+        this.firstElementChild.getAttribute('data-full-image')
+      );
+    });
+    // CLICK OPENS LIGHTBOX MODAL.
+    thumbnail[i].addEventListener("click", function() {
+      // TURNS LIGHTBOX 'ON' WITH IMG 'CLICK'.
+      lightBox.style.visibility = "visible";
+      Velocity(lightBox, "fadeIn", 800);
+      // UPDATES LIGHTBOX CAPTION TEXT FROM 'CLICKED' IMG [ATTR="ALT"]
+      lightboxAbout.textContent = this.getAttribute("alt");
 
-        });
+    });
 
-        // BUILDS AN ARRAY OF OBJECTS FROM THE .PHOTO ELEMENTS.
-        photoArr.push({
-            id: i,
-            src: thumbnail[i].getAttribute("data-full-image"),
-            text: thumbnail[i].getAttribute("alt").trim().toLowerCase(),
-            title: thumbnail[i].getAttribute("title").toLowerCase(),
-            active: true
-        });
+    // BUILDS AN ARRAY OF OBJECTS FROM THE .PHOTO ELEMENTS.
+    photoArr.push({
+      id: i,
+      src: thumbnail[i].getAttribute("data-full-image"),
+      text: thumbnail[i].getAttribute("alt").trim().toLowerCase(),
+      title: thumbnail[i].getAttribute("title").toLowerCase(),
+      active: true
+    });
 
-    }
+  }
 
 
-}() );
+}());
 
 // EVENT HANDLER FOR .closeBtn TURNS OFF LIGHTBOX.
 closeBtn.addEventListener("click", close);
@@ -165,21 +166,21 @@ nextBtn.addEventListener("click", nextPic);
 
 // KEYDOWN OF LEFT, RIGHT, ESC KEYS
 document.onkeydown = function() {
-    switch (window.event.keyCode) {
+  switch (window.event.keyCode) {
 
-        // LEFT ARROW
-        case 37:
-        prevPic();
-        break;
-        // RIGHT ARROW
-        case 39:
-        nextPic();
-        break;
-        // ESC KEY
-        case 27:
-        close();
-        break;
-    }
+    // LEFT ARROW
+    case 37:
+      prevPic();
+      break;
+      // RIGHT ARROW
+    case 39:
+      nextPic();
+      break;
+      // ESC KEY
+    case 27:
+      close();
+      break;
+  }
 };
 
 
